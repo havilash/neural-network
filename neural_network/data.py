db78be869b97ce49dd30748eac9f29d2c127f2ad
Havilash Sivaratnam <havilash2005@gmail.com>
2023-04-09 16:29:26 +0200

    feat: update data.py

--- neural_network/data.py
+++ neural_network/data.py
@@ -4,11 +4,8 @@ import numpy as np
 
 def get_mnist_data():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x = [*x_train, *x_test]
-    y = [*y_train, *y_test]
-    x = x_train.reshape(x_train.shape[0], 28, 28, 1)
-    x = x_train.astype('float32')
-    x /= 255
+    x, y = np.concatenate((x_train, x_test)), np.concatenate((y_train, y_test))
+    x = x.reshape(-1, 28, 28, 1).astype('float32') / 255
     return x, y
 
 def get_augmented_mnist_data(n):
@@ -17,27 +14,35 @@ def get_augmented_mnist_data(n):
         rotation_range=10,
         zoom_range=0.1,
         width_shift_range=0.1,
-        height_shift_range=0.1)
-    datagen.fit(x)
-    augmented_features = []
-    augmented_labels = []
-    for x_batch, y_batch in datagen.flow(x, y, batch_size=n):
-        augmented_features.append(x_batch)
-        augmented_labels.append(y_batch)
-        if len(augmented_features) == n:
-            break
-    return np.array(augmented_features), np.array(augmented_labels)
+        height_shift_range=0.1,
+        shear_range=5,
+        horizontal_flip=False,
+        vertical_flip=False,
+        fill_mode='nearest'
+    )
+    x = np.repeat(x, n, axis=0)
+    y = np.repeat(y, n)
+    for i in range(x.shape[0]):
+        x[i] = datagen.random_transform(x[i])
+    return x, y
 
-def get_train_test_data(data, train_size=0.7, shuffle=False):
-    x, y = data
+def train_test_split(x, y, test_size=0.2, shuffle=True):
     if shuffle:
-        indices = np.arange(X_train.shape[0])
+        indices = np.arange(x.shape[0])
         np.random.shuffle(indices)
-        X_train = X_train[indices]
-        y_train = y_train[indices]
-    split_index = int(train_size * len(X_train))
-    return (X_train[:split_index], y_train[:split_index]), (X_train[split_index:], y_train[split_index:]), (X_test, y_test)
+        x = x[indices]
+        y = y[indices]
+    split_index = int((1 - test_size) * x.shape[0])
+    x_train, x_test = x[:split_index], x[split_index:]
+    y_train, y_test = y[:split_index], y[split_index:]
+    return x_train, x_test, y_train, y_test
 
+def create_batches(data, batch_size):
+    for i in range(0, len(data), batch_size):
+        yield data[i:i+batch_size]
 
 if __name__ == "__main__":
-    print(get_mnist_data()[0][0])
+    print(len(get_mnist_data()[0]))
+    print(len(get_augmented_mnist_data(2)[0]))
+    x_train, x_test, y_train, y_test = train_test_split(*get_augmented_mnist_data(2))
+    print(len(x_train))
