1342967ac6e5e0a30fe1099033e62715a474ad30
Havilash Sivaratnam <havilash2005@gmail.com>
2023-04-15 21:57:27 +0200

    update

--- neural_network/main.py
+++ neural_network/main.py
@@ -5,27 +5,6 @@ import matplotlib.pyplot as plt
 from neural_network import activations, costs, layer, nn as neural_network
 from neural_network.data import get_mnist_data, get_augmented_mnist_data, train_test_split, create_batches
 
-data = [
-    ([-1, -2], [0, 1]),
-    ([-2, -1], [1, 0]),
-    ([-10, -2], [1, 0]),
-]
-
-def test_main():
-    nn = neural_network.NeuralNetwork([
-        layer.Layer(2, 8, activations.Identity), 
-        layer.Layer(8, 8, activations.Sigmoid),
-        layer.Layer(8, 2, activations.Sigmoid),
-    ])
-    
-    
-    for _ in range(1000):
-        nn.learn(data, .15)
-        # print(nn.calculate_outputs(data[0][0]))
-
-    for d in data:
-        print(nn.calculate_outputs(d[0]))
-    print(nn.cost([-2, -1], [1, 0]))
 
 def main():
     nn = neural_network.NeuralNetwork([
@@ -38,27 +17,30 @@ def main():
     x, y = get_mnist_data()
     x = x.reshape(-1, 28*28)
     y = np.array([one_hot(i) for i in y])
-    data = np.array(list(zip(x, y)))
+    x_train, x_test, y_train, y_test = train_test_split(x, y)
+    train_data = np.array(list(zip(x_train, y_train)))
+    test_data = np.array(list(zip(x_test, y_test)))
 
-    """for epoch in range(5):
-        for i, batch in enumerate(create_batches(data, 256)):
-            print(i)
-            print(nn.calculate_outputs(batch[0][0]), batch[0][1])
-            nn.learn((batch), .2)
+    nn.train(train_data, test_data, 0.2, batch_size=256, save=False)
     
-        with open('neural_network.pkl', 'wb') as f:
-            pickle.dump(nn, f)"""
-
     with open('neural_network.pkl', 'rb') as f:
-        nn: neural_network = pickle.load(f)
+        nn = neural_network.NeuralNetwork.load(f)
 
-    for _ in range(10):
-        i = np.random.randint(len(data))
-        plt.imshow(data[i][0].reshape((28, 28)), cmap='gray')
-        expected = np.argmax(data[i][1])
-        predicted = np.argmax(nn.calculate_outputs(data[i][0]))
-        plt.title(f"Expected: {expected}, Predicted: {predicted}")
-        plt.show()
+    print("Accuracy: ", nn.validate(test_data[:100]))
+
+    fig, axes = plt.subplots(3, 3, figsize=(6, 6))
+    for i in range(3):
+        for j in range(3):
+            k = np.random.randint(len(test_data))
+            axes[i, j].imshow(test_data[k][0].reshape((28, 28)), cmap='gray')
+            expected = np.argmax(test_data[k][1])
+            predicted = np.argmax(nn.predict(test_data[k][0]))
+            axes[i, j].set_title(f"Expected: {expected}\nPredicted: {predicted}")
+            axes[i, j].set_xticklabels([])
+            axes[i, j].set_yticklabels([])
+            axes[i, j].tick_params(axis='both', which='both', length=0)
+    fig.subplots_adjust(hspace=0.5)
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- neural_network/nn.py
+++ neural_network/nn.py
@@ -1,8 +1,10 @@
 import numpy as np
 import threading
+import pickle
 
 from neural_network.layer import Layer
 from neural_network import costs
+from neural_network.data import create_batches
 
 
 class NeuralNetwork:
@@ -12,14 +14,14 @@ class NeuralNetwork:
     def __repr__(self):
         return f"NeuralNetwork(\n layers={self.layers}\n)"
 
-    def calculate_outputs(self, inputs):
+    def predict(self, inputs):
         for layer in self.layers:
             inputs = layer.calculate_outputs(inputs)
         return inputs
     
     # Gradient Descent
     def cost(self, inputs, expected_outputs, node_cost: costs.Cost = costs.MeanSquaredError):
-        outputs = self.calculate_outputs(inputs) 
+        outputs = self.predict(inputs) 
         cost = np.sum(node_cost.func(outputs, expected_outputs))
         return cost
 
@@ -32,7 +34,7 @@ class NeuralNetwork:
             layer.apply_gradients(learn_rate)
 
     def update_gradients(self, inputs, expected_outputs, cost: costs.Cost):
-        self.calculate_outputs(inputs)
+        self.predict(inputs)
 
         output_layer = self.layers[-1]
         node_values = output_layer.caculate_output_layer_node_values(expected_outputs, cost)
@@ -43,7 +45,7 @@ class NeuralNetwork:
             node_values = hidden_layer.caculate_hidden_layer_node_values(self.layers[i+1], node_values)
             hidden_layer.update_gradients(node_values)
 
-    def learn(self, training_batch, learn_rate: float, cost: costs.Cost = costs.MeanSquaredError):
+    def learn(self, train_batch, learn_rate: float, cost: costs.Cost = costs.MeanSquaredError):
         """
         Update the gradients and apply them to the network based on a batch of training data.
 
@@ -53,7 +55,7 @@ class NeuralNetwork:
         """
         
         threads = []
-        for data in training_batch:
+        for data in train_batch:
             thread = threading.Thread(target=self.update_gradients, args=(data[0], data[1], cost))
             thread.start()
             threads.append(thread)
@@ -61,5 +63,62 @@ class NeuralNetwork:
         for thread in threads:
             thread.join()
 
-        self.apply_gradients(learn_rate / len(training_batch))
+        self.apply_gradients(learn_rate / len(train_batch))
         self.reset_gradients()
+
+    def train(self, train_data, test_data, learn_rate: float, cost: costs.Cost = costs.MeanSquaredError, batch_size: int = 32, epochs: int = 5, save: bool = False):
+        """
+        Update the gradients and apply them to the network based on a list of batches of training data.
+
+        :param data: A list of batches of training data. Each batch is a list of tuples containing input and target output data. Each tuple represents a single training example with the first element being the input data (features) and the second element being the target output data (label). For example: [[(x1, y1), (x2, y2)], [(x3, y3), (x4, y4)]] where x1, x2, x3, x4 are the input data and y1, y2, y3, y4 are the target output data.
+        :param learn_rate: The learning rate to use when applying the gradients.
+        :param cost: The cost function to use when calculating the error between the network's output and the target output.
+        :param save: Whether to save the model to a file after each epoch.
+        """
+        
+        if save: 
+            file = open('neural_network.pkl', 'wb')
+        num_batches = len(train_data) // batch_size
+        num = (num_batches // 50)
+        print()
+        print("Training ...")
+        for epoch in range(epochs):
+            print(f"Epoch {epoch+1}/{epochs}")
+            print("Progress: [", end="", flush=True)
+            epoch_cost = 0
+            for i, batch in enumerate(create_batches(train_data, batch_size)):
+                if i % num == 0:
+                    print("\u2588", end="", flush=True)
+                self.learn(batch, learn_rate)
+                epoch_cost += self.cost(batch[0][0], batch[0][1], cost)
+            epoch_cost /= num_batches
+            accuracy = self.validate(test_data)
+            print(f"] Cost: {epoch_cost:.4f} | Accuracy: {accuracy:.4f}")
+            if save: self.save(file)
+        if save: file.close()
+
+    def save(self, file):
+        pickle.dump(self, file)
+
+    @staticmethod
+    def load(file):
+        obj = pickle.load(file)
+        if isinstance(obj, NeuralNetwork):
+            return obj
+        else:
+            raise TypeError(f"Loaded object is not an instance of NeuralNetwork. Got {type(obj)} instead.")
+
+    def validate(self, data):
+        """
+        Calculate the accuracy of the model on the data.
+
+        :param data: A list of tuples containing input and target output data. Each tuple represents a single example with the first element being the input data (features) and the second element being the target output data (label).
+        :return: The accuracy of the model on the data.
+        """
+        correct_predictions = 0
+        for input, expected_output in data:
+            prediction = self.predict(input)
+            if np.argmax(prediction) == np.argmax(expected_output):
+                correct_predictions += 1
+        accuracy = correct_predictions / len(data)
+        return accuracy
